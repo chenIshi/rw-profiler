9ac40b02ece704d4f87708cb9b37c76e6a3a142a
chenishi <chen.ishi@gmail.com>
2020-04-12 17:56:12 +0800

    Add some analysis on io

--- src/io.c
+++ src/io.c
@@ -3,24 +3,42 @@
 #include <stdio.h>
 #include <dlfcn.h>
 
-// #define _GNU_SOURCE
+#include <sys/time.h>
+
+int read_count = 0;
+int write_count = 0;
 
 __ssize_t read(int __fd, void *__buf, size_t __nbytes) {
-	// char buf[32];
-	// sprintf(stdout, "Calling read: size %ld\n", __nbytes);
-	printf("Calling read: size %ld\n", __nbytes);
+	struct timeval start, end;
+	read_count ++;
 	static __ssize_t (*real_read)(int, void*, size_t) = NULL;
 	if (real_read == NULL) {
 		real_read = dlsym(RTLD_NEXT, "read");
 	}
-	return real_read(__fd, __buf, __nbytes);
+	gettimeofday(&start, NULL);
+	__ssize_t ret = real_read(__fd, __buf, __nbytes);
+	gettimeofday(&end, NULL);
+
+	long second = (end.tv_sec - start.tv_sec);
+	long micros = ((second * 1000000) + end.tv_usec - start.tv_usec);
+	printf("Calling read no.%d: size %ld, consumed time: %ld us\n", read_count, __nbytes, micros);
+	return ret;
 }
 
 __ssize_t write(int __fd, const void *__buf, size_t __n) {
-	printf("Calling write: size %ld\n", __n);
+	struct timeval start, end;
+	write_count ++;
 	static __ssize_t (*real_write)(int, const void*, size_t) = NULL;
 	if (real_write == NULL) {
 		real_write = dlsym(RTLD_NEXT, "write");
 	}
-	return real_write(__fd, __buf, __n);
+
+	gettimeofday(&start, NULL);
+	__ssize_t ret = real_write(__fd, __buf, __n);
+	gettimeofday(&end, NULL);
+
+	long second = (end.tv_sec - start.tv_sec);
+	long micros = ((second * 1000000) + end.tv_usec - start.tv_usec);
+	printf("Calling write no.%d: size %ld, consumed time: %ld us\n", read_count, __n, micros);
+	return ret;
 }
